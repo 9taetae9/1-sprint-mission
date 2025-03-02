5b949071db6119aafbab5aba2029480dafe916ff
TaeHyoun Kim <kth9903@gmail.com>
2025-03-03 00:12:10 +0900

    feat: 기본 요구사항 구현 완료

--- src/main/java/com/sprint/mission/discodeit/dto/response/PrivateChannelCreateResponse.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/PrivateChannelCreateResponse.java
@@ -2,17 +2,26 @@ package com.sprint.mission.discodeit.dto.response;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
+import java.time.Instant;
 import java.util.UUID;
 
 public record PrivateChannelCreateResponse(
     UUID id,
-    ChannelType type
+    Instant createdAt,
+    Instant updatedAt,
+    ChannelType type,
+    String name,
+    String description
 ) {
 
   public static PrivateChannelCreateResponse from(Channel channel) {
     return new PrivateChannelCreateResponse(
         channel.getId(),
-        channel.getType()
+        channel.getCreatedAt(),
+        channel.getUpdatedAt(),
+        channel.getType(),
+        channel.getName(),
+        channel.getDescription()
     );
   }
 }
